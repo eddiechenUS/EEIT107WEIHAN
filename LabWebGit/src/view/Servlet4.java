98acef44a16ac61b372edaba745dbaf7baafc1d7
陳威翰 <han@gmail.com>
2019-05-27 13:04:57 +0800

    第四次 更改, hello world 3

---
+++ LabWebGit/src/view/Servlet4.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet3 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		long time = System.currentTimeMillis();
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Servlet1:"+time+"</h3>");
+		System.out.println("hello world");
+		System.out.println("hello world2");
+		System.out.println("hello world3");
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+}
